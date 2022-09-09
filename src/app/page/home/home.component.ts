f2607cd1aff66475efacbc3ba5938121e2dc5485
MGamezG <l171130314@gamadero.tecnm.mx>
2022-09-09 13:46:50 -0500

    tiempos de vida

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges,DoCheck,AfterContentInit,
+AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges,DoCheck,AfterContentInit,
+AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy{
 tamano:number=10
 alert1:string='alert-info'
 alert2:string='warning'
@@ -13,10 +15,35 @@ properties:Object={
   dangers:true
 }
 loading:boolean=false
-  constructor() { }
+  constructor() {
+    console.log('constructor')
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit')
   }
+  ngOnChanges(){
+    console.log('ngOnChanges')
+  }
+  ngDoCheck(){
+    console.log('ngDoCheck')
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked')
+  }
+  ngOnDestroy(){
+    console.log('ngOnDestroy')
+  }
+
 
   ejecutar(){
     this.loading=true
